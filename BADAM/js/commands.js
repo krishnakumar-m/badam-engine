7cde229de859210cb66982c0de8bdc7943b1a972
Krishnakumar Muraleedharan <krishnakumar777@gmail.com>
2015-08-24 10:38:07 +0530

    script improvements

--- BADAM/js/commands.js
+++ BADAM/js/commands.js
@@ -2,15 +2,25 @@ function execCommand(line) {
 	var arr = line.split(" ");
 	switch (arr[0])
 	{
-		case "ADD_OBJ": addObjToScene(arr[1]);
+		case "ADD_OBJ": for(var i=1;i<arr.length;i++) {
+							addObjToScene(arr[i]);
+						}
 			break;
-		case "REMOVE_OBJ": remObjFromScene(arr[1]);
+		case "REMOVE_OBJ": for(var i=1;i<arr.length;i++) {
+								remObjFromScene(arr[i]);
+							}
 			break;
-		case "START_EVENT": addEvent(arr[1]);
+		case "START_EVENT": for(var i=1;i<arr.length;i++) {
+								addEvent(arr[i]);
+							}
 			break;
-		case "STOP_EVENT": removeEvent(arr[1]);
+		case "STOP_EVENT": for(var i=1;i<arr.length;i++) {
+								removeEvent(arr[i]);
+							}
 			break;
-		case "ADD_EXIT":unlock(arr[1],arr[2]);
+		case "ADD_EXIT":if(arr.length>2) {
+							unlock(arr[1],arr[2]);
+						}
 						addExit(arr[1]);
 			break;
 	}
